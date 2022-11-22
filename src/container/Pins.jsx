2b271b64a44fa53e78fe35c8e24d52444735889f
Roudi22 <roudi.alzkemi@gmail.com>
2022-11-23 00:54:09 +0100

    passing the user property

--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -7,7 +7,7 @@ const Pins = ({user}) => {
   return (
     <div className='px-2 md:px-5'>
       <div className='bg-gray-50'>
-        <Navbar searchterm={searchterm} setSearchterm={setSearchterm}/>
+        <Navbar searchterm={searchterm} setSearchterm={setSearchterm} user={user}/>
       </div>
       <div className='h-full'>
         <Routes>
@@ -16,6 +16,7 @@ const Pins = ({user}) => {
           <Route path='/pin-detail/:pinId' element={<PinDetail user={user}/>}/>
           <Route path='/create-pin' element={<CreatePin user={user}/>}/>
           <Route path='/search' element={<Search searchterm={searchterm} setSearchterm={setSearchterm}/>}/>
+
         </Routes>
       </div>
     </div>
