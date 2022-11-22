3e9e62ff3ecc94934172db748ca1287fb298a6ea
Roudi22 <roudi.alzkemi@gmail.com>
2022-11-22 22:38:12 +0100

    implementing Routes in Pins container

--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Routes, Route} from "react-router-dom"
 
-const Pins = () => {
+import { Navbar, Feed, PinDetail, CreatePin, Search} from "../components"
+const Pins = ({user}) => {
+  const [searchterm, setSearchterm] = useState("")
   return (
-    <div>Pins</div>
+    <div className='px-2 md:px-5'>
+      <div className='bg-gray-50'>
+        <Navbar searchterm={searchterm} setSearchterm={setSearchterm}/>
+      </div>
+      <div className='h-full'>
+        <Routes>
+          <Route path='/' element={<Feed/>}/>
+          <Route path='/category/:categoryId' element={<Feed/>}/>
+          <Route path='/pin-detail/:pinId' element={<PinDetail user={user}/>}/>
+          <Route path='/create-pin' element={<CreatePin user={user}/>}/>
+          <Route path='/search' element={<Search searchterm={searchterm} setSearchterm={setSearchterm}/>}/>
+        </Routes>
+      </div>
+    </div>
   )
 }
 
