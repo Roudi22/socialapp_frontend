ac1ab9ba5fbf5ac0d0c47a9dda4446189a57e212
Roudi22 <roudi.alzkemi@gmail.com>
2022-11-27 19:30:02 +0100

    create and implement search functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import MasonryLayout from './MasonryLayout'
+import { client } from '../client'
+import { feedQuery, searchQuery } from '../utils/data'
+import Spinner from './Spinner'
 
-const Search = () => {
+const Search = ({ searchterm }) => {
+  const [pins, setPins] = useState(null)
+  const [loading, setLoading] = useState(false)
+  
+  useEffect(() => {
+    if(searchterm) {
+      setLoading(true);
+      const query = searchQuery(searchterm.toLowerCase());
+
+      client.fetch(query)
+      .then((data)=> {
+        setPins(data);
+        setLoading(false);
+      })
+    } else {
+      client.fetch(feedQuery)
+      .then((data)=> {
+        setPins(data);
+        setLoading(false);
+      })
+    }
+  }, [searchterm])
+  
+
   return (
-    <div>Search</div>
+    <div>
+      {loading && <Spinner message="Searching for pins..."/>}
+      {pins?.length !== 0 && <MasonryLayout pins={pins}/>}
+      {pins?.length === 0 && searchterm !== "" && !loading && (
+        <div className='mt-10 text-center text-xl'>No Pins Found</div>
+      )}
+    </div>
   )
 }
 
