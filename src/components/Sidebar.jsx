03d5fcfd3bb6fd9a94b60f753e5067daad0371d3
Roudi22 <roudi.alzkemi@gmail.com>
2022-11-27 19:30:19 +0100

    add categories from data file

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { RiHomeFill } from "react-icons/ri";
 import { IoIosArrowForward } from "react-icons/io";
 
 import logo from "../assets/logo.png";
-
+import { categories } from "../utils/data";
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
     if (closeToggle) closeToggle(false);
@@ -15,15 +15,7 @@ const Sidebar = ({ user, closeToggle }) => {
   const isActiveStyle =
     "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
 
-  const categories = [
-    { name: "Animals" },
-    { name: "Wallapapers" },
-    { name: "Photography" },
-    { name: "Gaming" },
-    { name: "Coding" },
-    { name: "Other" },
-  ];
-
+  
   return (
     <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
       <div className="flex flex-col">
@@ -57,6 +49,7 @@ const Sidebar = ({ user, closeToggle }) => {
               onClick={handleCloseSidebar}
               key={category.name}
             >
+                <img src={category.image} className="w-8 h-8 rounded-full shadow-sm" alt="category" />
                 {category.name}
             </NavLink>
           ))}
