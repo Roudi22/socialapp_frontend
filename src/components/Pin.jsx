11a1d18234ad18e2d9d90d7e030e15e1a8731e44
Roudi22 <roudi.alzkemi@gmail.com>
2022-11-27 19:29:23 +0100

    some fixes

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { urlFor, client } from "../client";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import { MdDownloadForOffline } from "react-icons/md";
 import { AiTwotoneDelete } from "react-icons/ai";
